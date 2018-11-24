50dfcd0994e256dd4b38eac684d2c52248f126a8
ssong-eu <sangyoon.song@gmail.com>
2018-11-24 17:11:57 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,41 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            +`api-key=${NYT_API_KEY}&query=`
+         
 
-// Code SearchableMovieReviewsContainer Here
+            export default class LatestMovieReviewsContainer extends Component {
+                constructor() {
+                    super();
+                
+                    this.state = {
+                      reviews: [],
+                      searchTerm: ""
+                    };
+                  }
+
+                handleChange = (event) => {
+                    this.setState({ searchTerm: event.target.value})
+                }
+            
+                onSubmit() {
+                    fetch(URL + this.state.searchTerm)
+                        .then(resp => resp.json())
+                        .then(reviews => this.setState({ reviews: reviews.results }))
+                }
+            
+                render () {
+                    return ( 
+                        <div className="searchable-movie-reviews">
+                            <form className="search-form" onSubmit={() => this.onSubmit()}>
+                                <input type="text" placeholder="movie title" onChange={(event) => this.handleChange(event)} />
+                                <input type="submit" name="submit" />
+                            </form>
+                            <h2>The Searched Reviews:</h2>
+                            <MovieReviews reviews={ this.state.reviews } />
+                        </div>
+                    )
+                }
+            }
+            
